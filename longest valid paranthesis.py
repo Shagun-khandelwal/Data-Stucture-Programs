48729dcea1bdce48b8086b202288159a029c7e03
shagun-khandelwal <kshagun02@gmail.com>
2022-08-09 02:39:38 +0530

    longest valid paranthesis added

---
+++ longest valid paranthesis.py
@@ -0,0 +1,18 @@
+def maxLength(S):
+    # code here
+    stack = [0]
+    longest = 0
+    for x in S:
+        if x == "(":
+            stack.append(0)
+        else:
+            if len(stack) > 1:
+                val = stack.pop()
+                stack[-1] = stack[-1] + val + 2
+                longest = max(longest, stack[-1])
+            else:
+                stack = [0]
+    return longest
+
+myinput="(())()("
+print(maxLength(myinput))
