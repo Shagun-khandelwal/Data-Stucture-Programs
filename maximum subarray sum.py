7834e90d7d0bb796e99378c8953e27fefad69d1a
shagun-khandelwal <kshagun02@gmail.com>
2022-08-09 00:52:44 +0530

    maximum subarray sum added

---
+++ maximum subarray sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        max_sum=nums[0]
+        curr_sum=0
+        for x in nums:
+            if curr_sum<0:
+                curr_sum=0
+            curr_sum+=x
+            max_sum = max(max_sum,curr_sum)
+        return max_sum
+if __name__ =='__main__':
+    nums=[-2,1,-3,4,-1,2,1,-5,4]
+    Sol = Solution()
+    result = Sol.maxSubArray(nums)
+    print(result)
